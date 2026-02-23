8cac72a529878ec9462a140b7b59596863355b5f
Golam Rabbani <rabbanug@gmail.com>
2026-02-23 15:27:08 +0600

    feat: Introduce Dockerization for the web application, configuring Nginx, PHP-FPM, and Supervisor, and updating the README with Docker instructions.

---
+++ web/docker/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# If a .env file is mapped but empty, or if we want to run setup tasks
+# we can do it here
+
+# Wait for DB if necessary (e.g. if DB_HOST is set)
+
+# Make sure permissions are correct just in case volume mounts changed them
+chown -R nobody:nobody /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/database 2>/dev/null
+chmod -R 777 /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/database 2>/dev/null
+
+# Start supervisord to run php-fpm and nginx
+exec /usr/bin/supervisord -c /etc/supervisord.conf
